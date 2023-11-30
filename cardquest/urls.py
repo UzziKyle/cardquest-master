cd4e9ce62d479c862ab29a5de4569829dff230b6
UzziKyle <uzzielkyleynciong0824@gmail.com>
2023-12-01 00:01:30 +0800

    add views

--- cardquest/urls.py
+++ cardquest/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from . import views
+from cardquest.views import HomePageView, TrainerList
+
 
 urlpatterns = [
-    
+    path('', views.HomePageView.as_view(), name='home'),
+    path('trainer_list', TrainerList.as_view(), name='trainer-list'),
 ]
--- cardquest/views.py
+++ cardquest/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from typing import Any
+from django.views.generic.list import ListView
+from cardquest.models import PokemonCard, Trainer
+
 
 # Create your views here.
+class HomePageView(ListView):
+    model = PokemonCard
+    context_object_name = 'home'
+    template_name = 'base.html'
+    
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        return super().get_context_data(**kwargs)
+    
+    
+class TrainerList(ListView):
+    model = Trainer
+    context_object_name = 'trainer'
+    template_name = 'trainers.html'
+    paginate_by = 15    
+    
