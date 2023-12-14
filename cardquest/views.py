4a85d4d850b2adc0765559946e327c6380d444c5
UzziKyle <uzzielkyleynciong0824@gmail.com>
2023-12-14 10:08:20 +0800

    add pagination

--- cardquest/views.py
+++ cardquest/views.py
@@ -11,6 +11,7 @@ class HomePageView(ListView):
     model = PokemonCard
     context_object_name = 'home'
     template_name = 'home.html'
+    paginate_by = 15  
     
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         return super().get_context_data(**kwargs)
@@ -41,7 +42,7 @@ class TrainerList(ListView):
     model = Trainer
     context_object_name = 'trainer'
     template_name = 'trainer.html'
-    paginate_by = 15   
+    paginate_by = 15
     
     
 class TrainerCreateView(CreateView):
@@ -68,12 +69,12 @@ class PokemonCardList(ListView):
     model = PokemonCard
     context_object_name = 'pokemon-card'
     template_name = 'pokemon_card.html'
-    paginate_by = 15    
+    paginate_by = 15 
 
     
 class CollectionList(ListView):
     model = Collection
     context_object_name = 'collection'
     template_name = 'collections.html'
-    paginate_by = 15    
+    paginate_by = 15
     
